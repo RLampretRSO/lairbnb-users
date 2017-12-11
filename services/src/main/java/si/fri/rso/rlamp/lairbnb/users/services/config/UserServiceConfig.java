537ed576144cc0c56c7e71acc9f876254a4d75d9
Rok Lampret <rl7811@student.uni-lj.si>
2017-12-12 00:08:48 +0100

    Adding missing class

---
+++ services/src/main/java/si/fri/rso/rlamp/lairbnb/users/services/config/UserServiceConfig.java
@@ -0,0 +1,55 @@
+package si.fri.rso.rlamp.lairbnb.users.services.config;
+
+import com.kumuluz.ee.configuration.cdi.ConfigBundle;
+import com.kumuluz.ee.configuration.cdi.ConfigValue;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+@ConfigBundle("properties")
+public class UserServiceConfig {
+
+    @ConfigValue(value = "external-services.reservation-service.enabled", watch = true)
+    private boolean reservationServiceEnabled;
+
+    @ConfigValue(value = "external-services.lair-service.enabled", watch = true)
+    private boolean lairServiceEnabled;
+
+    @ConfigValue(value = "service.persistence.create.allowed", watch = true)
+    private boolean allowRegisteringNewUsers;
+
+    @ConfigValue(value = "service.persistence.delete.allowed", watch = true)
+    private boolean allowDeletingUsers;
+
+    public boolean isReservationServiceEnabled() {
+        return reservationServiceEnabled;
+    }
+
+    public void setReservationServiceEnabled(boolean reservationServiceEnabled) {
+        this.reservationServiceEnabled = reservationServiceEnabled;
+    }
+
+    public boolean isLairServiceEnabled() {
+        return lairServiceEnabled;
+    }
+
+    public void setLairServiceEnabled(boolean lairServiceEnabled) {
+        this.lairServiceEnabled = lairServiceEnabled;
+    }
+
+    public boolean isAllowRegisteringNewUsers() {
+        return allowRegisteringNewUsers;
+    }
+
+    public void setAllowRegisteringNewUsers(boolean allowRegisteringNewUsers) {
+        this.allowRegisteringNewUsers = allowRegisteringNewUsers;
+    }
+
+    public boolean isAllowDeletingUsers() {
+        return allowDeletingUsers;
+    }
+
+    public void setAllowDeletingUsers(boolean allowDeletingUsers) {
+        this.allowDeletingUsers = allowDeletingUsers;
+    }
+}
